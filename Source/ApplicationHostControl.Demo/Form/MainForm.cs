e6df9af67a8cef0dc70b415355fb709be1349125
larrynung <Larry.Nung@TWDT092.xuenn.com>
2013-12-10 10:48:57 +0800

    Use .NET Framework 3.5 & Adjust code waitting for process ready

--- Source/ApplicationHostControl.Demo/Form/MainForm.cs
+++ Source/ApplicationHostControl.Demo/Form/MainForm.cs
@@ -1,36 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ApplicationHostControl.Demo
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			applicationHost1.File = "calc.exe";
-			applicationHost1.HideApplicationTitleBar = true;
-		}
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            applicationHost1.File = "calc.exe";
+            applicationHost1.HideApplicationTitleBar = true;
+        }
 
-		private void btnLoad_Click(object sender, EventArgs e)
-		{
-			applicationHost1.LoadProcess();
-		}
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            applicationHost1.LoadProcess();
+        }
 
-		private void btnUnLoad_Click(object sender, EventArgs e)
-		{
-			applicationHost1.UnloadProcess();
-		}
-	}
+        private void btnUnLoad_Click(object sender, EventArgs e)
+        {
+            applicationHost1.UnloadProcess();
+        }
+    }
 }
